f32ee742b86fee56f4ce9800fc12195cf6e02f80
rohanpaul <rohanp@csail.mit.edu>
2016-10-26 10:27:55 -0400

    object property file names

--- src/symbol/h2sl/object_property.h
+++ src/symbol/h2sl/object_property.h
@@ -37,6 +37,7 @@ namespace h2sl {
     virtual void to_xml( const std::string& file )const;
     virtual void to_xml( xmlDocPtr doc, xmlNodePtr root )const;
 
+    /*
     inline std::string& object_type( void ){ return get_prop< std::string >( _properties, "object_type" ); };
     inline const std::string& object_type( void )const{ return get_prop< std::string >( _properties, "object_type" ); };
     
@@ -45,6 +46,16 @@ namespace h2sl {
     
     inline std::string& index_type( void ){ return get_prop< std::string >( _properties, "index_type" ); };
     inline const std::string& index_type( void )const{ return get_prop< std::string >( _properties, "index_type" ); };
+    */
+    inline std::string& type( void ){ return get_prop< std::string >( _properties, "object_type" ); };
+    inline const std::string& type( void )const{ return get_prop< std::string >( _properties, "object_type" ); };
+    
+    inline std::string& relation_type( void ){ return get_prop< std::string >( _properties, "spatial_relation_type" ); };
+    inline const std::string& relation_type( void )const{ return get_prop< std::string >( _properties, "spatial_relation_type" ); };
+    
+    inline std::string& index( void ){ return get_prop< std::string >( _properties, "index_type" ); };
+    inline const std::string& index( void )const{ return get_prop< std::string >( _properties, "index_type" ); };
+
 
   protected:
 
