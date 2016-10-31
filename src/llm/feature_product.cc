9fa2c03e2a154440abcab3d3df94d55b7121eebb
Jake Arkin <jarkin@ur.rochester.edu>
2016-10-31 17:16:45 -0400

    updated files to match adcg branch for merging

--- src/llm/feature_product.cc
+++ src/llm/feature_product.cc
@@ -78,7 +78,7 @@
 #include "h2sl/feature_container_merge_container_spatial_relation.h"
 #include "h2sl/feature_region_container_merge_container_spatial_relation.h"
 #include "h2sl/feature_region_container_container_matches_child_container.h"
-//#include "h2sl/feature_object_property_index.h"
+#include "h2sl/feature_object_property_index.h"
 #include "h2sl/feature_region_abstract_container_type.h"
 #include "h2sl/feature_region_abstract_container_object_type.h"
 #include "h2sl/feature_region_abstract_container_number.h"
@@ -408,8 +408,9 @@ from_xml( xmlNodePtr root ){
               } else if ( xmlStrcmp( l2->name, ( const xmlChar* )( "feature_region_container_container_matches_child_container" ) ) == 0 ){
                   _feature_groups.back().push_back( new Feature_Region_Container_Container_Matches_Child_Container() );
                   _feature_groups.back().back()->from_xml( l2 );
-              //} else if ( xmlStrcmp( l2->name, ( const xmlChar* )( "feature_object_property_index" ) ) == 0 ){
-              //    _feature_groups.back().push_back( new Feature_Object_Property_Index( l2 ) );
+              } else if ( xmlStrcmp( l2->name, ( const xmlChar* )( "feature_object_property_index" ) ) == 0 ){
+                  _feature_groups.back().push_back( new Feature_Object_Property_Index() );
+                  _feature_groups.back().back()->from_xml( l2 );
               } else if ( xmlStrcmp( l2->name, ( const xmlChar* )( "feature_region_abstract_container_type" ) ) == 0 ){
                   _feature_groups.back().push_back( new Feature_Region_Abstract_Container_Type() );
                   _feature_groups.back().back()->from_xml( l2 );
