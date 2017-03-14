0b8d62ee72ade7720d78be017f919800a063aeff
Thomas Howard <thomas.howard@rochester.edu>
2017-03-14 09:10:40 -0400

    added first rules for new HDCG implementation

---
+++ src/symbol/h2sl/rule_object_type.h
@@ -0,0 +1,61 @@
+/**
+ * @file rule_object_type.h
+ * 
+ * @brief
+ * 
+ * a class used to describe a rule for a spatial relationship
+ */
+
+#ifndef H2SL_RULE_OBJECT_TYPE_H
+#define H2SL_RULE_OBJECT_TYPE_H
+
+#include <iostream>
+#include <libxml/tree.h>
+
+#include "h2sl/symbol_dictionary.h"
+#include "h2sl/world.h"
+#include "h2sl/rule.h"
+
+namespace h2sl {
+  /**
+   * Rule_Object_Type class definition
+   */
+  class Rule_Object_Type : public Rule {
+  public:
+
+    Rule_Object_Type( const std::string& objectType = "na" );
+    Rule_Object_Type( const Rule_Object_Type& other );
+    Rule_Object_Type( xmlNodePtr root );
+    virtual ~Rule_Object_Type();
+    Rule_Object_Type& operator=( const Rule_Object_Type& other );
+    bool operator==( const Rule_Object_Type& other )const;
+    bool operator!=( const Rule_Object_Type& other )const;
+    virtual Rule_Object_Type* dup( void )const;
+
+    virtual bool matches_class_name( const std::string& arg )const{ return ( arg == "rule_object_type" ); };
+    virtual void scrape_grounding( const World * world, std::vector< std::string >& classNames, std::map< std::string, std::vector< std::string > >& stringTypes, std::map< std::string, std::vector< int > >& intTypes )const;
+    static void fill_search_space( const Symbol_Dictionary& symbolDictionary, const World* world, std::map< std::string, std::pair< std::string, std::vector< Grounding* > > >& searchSpaces, const symbol_type_t& symbolType );
+ 
+    virtual void from_xml( const std::string& file );
+    virtual void from_xml( xmlNodePtr root );
+    virtual void to_xml( const std::string& file )const;
+    virtual void to_xml( xmlDocPtr doc, xmlNodePtr root )const;
+
+    inline std::string& object_type( void ){ return get_prop< std::string >( _string_properties, "object_type" ); };
+    inline const std::string& object_type( void )const{ return get_prop< std::string >( _string_properties, "object_type" ); };
+
+    static std::string class_name( void ){ return "rule_object_type"; };
+
+  protected:
+
+  private:
+
+  };
+
+  /** 
+   * Rule_Object_Type class ostream operator
+   */
+  std::ostream& operator<<( std::ostream& out, const Rule_Object_Type& other );
+}
+
+#endif /* H2SL_RULE_OBJECT_TYPE_H */
---
+++ src/symbol/h2sl/rule_set.h
@@ -0,0 +1,79 @@
+/**
+ * @file    rule_set.h
+ * @author  Thomas M. Howard (tmhoward@csail.mit.edu)
+ *          Matthew R. Walter (mwalter@csail.mit.edu)
+ * @version 1.0
+ *
+ * @section LICENSE
+ *
+ * This file is part of h2sl.
+ *
+ * Copyright (C) 2014 by the Massachusetts Institute of Technology
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html> or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ *
+ * @section DESCRIPTION
+ *
+ * The interface for a class used to describe a set of rules
+ */
+
+#ifndef H2SL_RULE_SET_H
+#define H2SL_RULE_SET_H
+
+#include <iostream>
+#include <vector>
+
+#include "h2sl/rule.h"
+
+namespace h2sl {
+  class Rule_Set: public Rule {
+  public:
+    Rule_Set( const std::vector< Rule* >& rules = std::vector< Rule* >() );
+    Rule_Set( xmlNodePtr root );
+    virtual ~Rule_Set();
+    Rule_Set( const Rule_Set& other );
+    Rule_Set& operator=( const Rule_Set& other );
+    virtual Rule_Set* dup( void )const;
+
+    virtual bool matches_class_name( const std::string& arg )const{ return ( arg == "rule_set" ); };
+    virtual void scrape_grounding( const World * world, std::vector< std::string >& classNames, std::map< std::string, std::vector< std::string > >& stringTypes, std::map< std::string, std::vector< int > >& intTypes )const;
+
+    void clear( void );
+ 
+    virtual unsigned int evaluate_cv( const h2sl::Grounding* rule )const;
+
+    virtual void to_xml( const std::string& filename )const;
+    virtual void to_xml( xmlDocPtr doc, xmlNodePtr root )const;
+
+    virtual void from_xml( const std::string& filename );
+    virtual void from_xml( xmlNodePtr root );
+
+    inline std::vector< Rule* >& rules( void ){ return _rules; };
+    inline const std::vector< Rule* >& rules( void )const{ return _rules; };
+
+    static std::string class_name( void ){ return "rule_set"; };
+
+  protected:
+    std::vector< Rule* > _rules;
+
+  private:
+
+  };
+  std::ostream& operator<<( std::ostream& out, const Rule_Set& other );
+}
+
+#endif /* H2SL_RULE_SET_H */
