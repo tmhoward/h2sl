c1cba9cf34880a090d968b3cbd07f3f9e90b65f3
Thomas Howard <thomas.howard@rochester.edu>
2017-03-18 13:49:58 -0400

    added container_type symbol, updated annotations for new symbol, fixed annotation errors

--- src/symbol/container_type.cc
+++ src/symbol/container_type.cc
@@ -1,66 +1,68 @@
 /**
- * @file rule_container_type.cc
+ * @file container_type.cc
  *
  * @brief
  *
- * a class used to describe a spatial relationship
+ * a class that describes a container_type
  */
 
-#include <assert.h>
-
 #include "h2sl/rule_container_type.h"
+#include "h2sl/container_type.h"
+#include "h2sl/world.h"
 
 using namespace std;
 using namespace h2sl;
 
 /**
- * Rule_Container_Type class constructor
+ * Container_Type class constructor
  */
-Rule_Container_Type::
-Rule_Container_Type( const string& containerType ) : Rule() {
+Container_Type::
+Container_Type( const string& containerType ) : Grounding() {
   insert_prop< std::string >( _string_properties, "container_type", containerType );
 }
 
-Rule_Container_Type::
-Rule_Container_Type( xmlNodePtr root ) : Rule() {
-  insert_prop< std::string >( _string_properties, "container_type", "na" );
-  from_xml( root );
+
+Container_Type::
+Container_Type( xmlNodePtr root ) : Grounding() {
+    insert_prop< std::string >( _string_properties, "container_type", "na" );
+    from_xml( root );
+}
+
+
+/**
+ * Container_Type class copy constructor
+ */
+Container_Type::
+Container_Type( const Container_Type& other ) : Grounding( other ) {
+
 }
 
 /**
- * Rule_Container_Type class copy constructor
+ * Container_Type class destructor
  */
-Rule_Container_Type::
-Rule_Container_Type( const Rule_Container_Type& other ) : Rule( other ) {
-
-}
-
-/**
- * Rule_Container_Type class destructor
- */
-Rule_Container_Type::
-~Rule_Container_Type() {
+Container_Type::
+~Container_Type() {
 
 }
 
 /** 
- * Rule_Container_Type class assignment operator
+ * Container_Type class assignment operator
  */
-Rule_Container_Type&
-Rule_Container_Type::
-operator=( const Rule_Container_Type& other ){
+Container_Type&
+Container_Type::
+operator=( const Container_Type& other ){
   _string_properties = other._string_properties;
   _int_properties = other._int_properties;
   return (*this);
 }
 
 /** 
- * Rule_Container_Type class equality operator
+ * Container_Type class equality operator
  */
 bool
-Rule_Container_Type::
-operator==( const Rule_Container_Type& other )const{
-  if ( container_type() != other.container_type() ){
+Container_Type::
+operator==( const Container_Type& other )const{
+  if ( type() != other.type() ){
     return false;
   } else {
     return true;
@@ -68,30 +70,30 @@ operator==( const Rule_Container_Type& other )const{
 }
 
 /** 
- * Rule_Container_Type class inequality operator
+ * Container_Type class inequality operator
  */
 bool
-Rule_Container_Type::
-operator!=( const Rule_Container_Type& other )const{
+Container_Type::
+operator!=( const Container_Type& other )const{
   return !( *this == other );
 }
 
 /** 
- * Rule_Container_Type class dup operator
+ * Container_Type class dup operator
  */
-Rule_Container_Type*
-Rule_Container_Type::
+Container_Type*
+Container_Type::
 dup( void )const{
-  return new Rule_Container_Type( *this );
+  return new Container_Type( *this );
 }
 
 string
-Rule_Container_Type::
+Container_Type::
 evaluate_cv( const Grounding_Set* groundingSet )const{
   string cv = "false";
   for( unsigned int i = 0; i < groundingSet->groundings().size(); i++ ){
-    if( dynamic_cast< const Rule_Container_Type* >( groundingSet->groundings()[ i ] ) ){
-      if( *this == *static_cast< const Rule_Container_Type* >( groundingSet->groundings()[ i ] ) ){
+    if( dynamic_cast< const Container_Type* >( groundingSet->groundings()[ i ] ) ){
+      if( *this == *static_cast< const Container_Type* >( groundingSet->groundings()[ i ] ) ){
         cv = "true";
       }
     }
@@ -100,16 +102,16 @@ evaluate_cv( const Grounding_Set* groundingSet )const{
 }
 
 void
-Rule_Container_Type::
+Container_Type::
 scrape_grounding( const World * world,
                   map< string, vector< string > >& stringTypes,
                   map< string, vector< int > >& intTypes )const{
-  insert_unique< std::string >( "container_type", container_type(), stringTypes );
+  insert_unique< std::string >( "container_type", type(), stringTypes );
   return;
 }
 
 void
-Rule_Container_Type::
+Container_Type::
 scrape_grounding( const World * world,
                   vector< string >& classNames,
                   map< string, vector< string > >& stringTypes,
@@ -120,13 +122,13 @@ scrape_grounding( const World * world,
 }
 
 void
-Rule_Container_Type::
+Container_Type::
 fill_search_space( const Symbol_Dictionary& symbolDictionary,
                     const World* world,
                     map< string, pair< string, vector< Grounding* > > >& searchSpaces,
                     const std::string& symbolType ){
 
-  if( symbolDictionary.has_class_name( class_name() ) || symbolDictionary.has_class_name( "container_type" ) || symbolDictionary.has_class_name( "container" ) ){
+  if( symbolDictionary.has_class_name( class_name() ) ){
     map< string, pair< string, vector< Grounding* > > >::iterator it_search_spaces_symbol = searchSpaces.find( class_name() );
     if( it_search_spaces_symbol == searchSpaces.end() ){
       searchSpaces.insert( pair< string, pair< string, vector< Grounding* > > >( class_name(), pair< string, vector< Grounding* > >( "binary", vector< Grounding* >() ) ) );
@@ -138,7 +140,7 @@ fill_search_space( const Symbol_Dictionary& symbolDictionary,
     if( ( symbolType == "concrete" ) || ( symbolType == "all" ) ){
       if( it_container_type_types != symbolDictionary.string_types().end() ){
         for( unsigned int i = 0; i < it_container_type_types->second.size(); i++ ){
-          it_search_spaces_symbol->second.second.push_back( new Rule_Container_Type( it_container_type_types->second[ i ] ) );
+          it_search_spaces_symbol->second.second.push_back( new Container_Type( it_container_type_types->second[ i ] ) );
         }
       }
     }
@@ -148,18 +150,18 @@ fill_search_space( const Symbol_Dictionary& symbolDictionary,
 }
 
 void
-Rule_Container_Type::
+Container_Type::
 fill_rules( const World* world, Grounding_Set* groundingSet )const{
-  Rule_Container_Type rule_container_type( container_type() );
+  Rule_Container_Type rule_container_type( type() );
   insert_unique_grounding< Rule_Container_Type >( groundingSet, rule_container_type );
   return;
 }
 
 /** 
- * imports the Rule_Container_Type class from an XML file
+ * imports the Container_Type class from an XML file
  */
 void
-Rule_Container_Type::
+Container_Type::
 from_xml( const string& filename ){
   xmlDoc * doc = NULL;
   xmlNodePtr root = NULL;
@@ -170,7 +172,7 @@ from_xml( const string& filename ){
       xmlNodePtr l1 = NULL;
       for( l1 = root->children; l1; l1 = l1->next ){
         if( l1->type == XML_ELEMENT_NODE ){
-          if( xmlStrcmp( l1->name, ( const xmlChar* )( "rule_container_type" ) ) == 0 ){
+          if( xmlStrcmp( l1->name, ( const xmlChar* )( "container_type" ) ) == 0 ){
             from_xml( l1 );
           }
         }
@@ -184,32 +186,33 @@ from_xml( const string& filename ){
 }
 
 /** 
- * imports the Rule_Container_Type class from an XML node pointer
+ * imports the Container_Type class from an XML node pointer
  */
 void
-Rule_Container_Type::
+Container_Type::
 from_xml( xmlNodePtr root ){
-  container_type() = "na";
+  type() = "na";
   if( root->type == XML_ELEMENT_NODE ){
-    vector< string > container_type_keys = { "type", "container_type" };
-    assert( check_keys( root, container_type_keys ) );
+    vector< string > container_keys = { "type", "container_type" };
+    assert( check_keys( root, container_keys ) );
+
     pair< bool, string > container_type_prop = has_prop< std::string >( root, "container_type" );
     if( container_type_prop.first ){
-      container_type() = container_type_prop.second;
+      type() = container_type_prop.second;
     }
     container_type_prop = has_prop< std::string >( root, "type" );
     if( container_type_prop.first ){
-      container_type() = container_type_prop.second;
+      type() = container_type_prop.second;
     }
   }
   return;
 }
 
 /**
- * exports the Rule_Container_Type class to an XML file
+ * exports the Container_Type class to an XML file
  */
 void
-Rule_Container_Type::
+Container_Type::
 to_xml( const string& filename )const{
   xmlDocPtr doc = xmlNewDoc( ( xmlChar* )( "1.0" ) );
   xmlNodePtr root = xmlNewDocNode( doc, NULL, ( xmlChar* )( "root" ), NULL );
@@ -221,28 +224,27 @@ to_xml( const string& filename )const{
 }
 
 /**
- * exports the Rule_Container_Type class to an XML node pointer
+ * exports the Container_Type class to an XML node pointer
  */
 void
-Rule_Container_Type::
+Container_Type::
 to_xml( xmlDocPtr doc,
         xmlNodePtr root )const{
-  xmlNodePtr node = xmlNewDocNode( doc, NULL, ( xmlChar* )( "rule_container_type" ), NULL );
-  xmlNewProp( node, ( const xmlChar* )( "container_type" ), ( const xmlChar* )( get_prop< std::string >( _string_properties, "container_type" ).c_str() ) );
+  xmlNodePtr node = xmlNewDocNode( doc, NULL, ( xmlChar* )( "container_type" ), NULL );
+  xmlNewProp( node, ( const xmlChar* )( "container_type" ), ( const xmlChar* )( get_prop< std::string >( _string_properties, "container_type").c_str() ) );
   xmlAddChild( root, node );
   return;
 }
 
 namespace h2sl {
   /** 
-   * Rule_Container_Type class ostream operator
+   * Container_Type class ostream operator
    */
   ostream&
   operator<<( ostream& out,
-              const Rule_Container_Type& other ){
-    out << "Rule_Container_Type(";
-    out << "container_type=\"" << other.container_type() << "\"";
-    out << ")";
+              const Container_Type& other ){
+    out << "class:\"Container_Type\" ";
+    out << "container_type:" << other.type() << " ";
     return out;
   }
 }
---
+++ src/symbol/container_type_demo.cc
@@ -0,0 +1,111 @@
+/**
+ * @file    container_type_demo.cc
+ * @author  Thomas M. Howard (tmhoward@csail.mit.edu)
+ *          Matthew R. Walter (mwalter@csail.mit.edu)
+ * @version 1.0
+ *
+ * @section LICENSE
+ *
+ * This file is part of h2sl.
+ *
+ * Copyright (C) 2014 by the Massachusetts Institute of Technology
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html> or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ *
+ * @section DESCRIPTION
+ *
+ * An Container_Type class demo program
+ */
+
+#include <iostream>
+#include <cstdlib>
+#include "h2sl/container_type.h"
+#include "container_type_demo_cmdline.h"
+
+using namespace std;
+using namespace h2sl;
+
+void
+fill_container_types( vector< Container_Type* >& container_types, const string filename ){
+  xmlDoc * doc = NULL;
+  xmlNode * root = NULL;
+  doc = xmlReadFile( filename.c_str(), NULL, 0 );
+  if( doc != NULL ){
+    root = xmlDocGetRootElement( doc );
+    if( root->type == XML_ELEMENT_NODE ){
+      for( xmlNodePtr l1 = root->children; l1; l1 = l1->next ){
+        if( l1->type == XML_ELEMENT_NODE ){
+          if( xmlStrcmp( l1->name, ( const xmlChar* )( "container_type" ) ) == 0 ){
+            container_types.push_back( new Container_Type( l1 ) );
+          }
+        }
+      }
+    }
+  }
+  return;
+}
+
+int
+main( int argc,
+      char* argv[] ) {
+  cout << "start of Container_Type class demo program" << endl;
+
+  gengetopt_args_info args;
+  if( cmdline_parser( argc, argv, &args ) != 0 ){
+    exit(1);
+  }
+
+  vector< Container_Type* > container_types;
+
+  if( args.input_given ){
+    fill_container_types( container_types, args.input_arg );
+  }
+
+  if( !container_types.empty() ){
+    cout << "container_types.back()->class_name(): " << container_types.back()->class_name() << endl;
+    for( unsigned int i = 0; i < container_types.size(); i++ ){
+      cout << "container_types[ " << i << " ]: " << *container_types[ i ] << endl;
+    }
+  } else{
+    cout << "the input did not have examples of container_type, so we just demo the static functions and ostream operator for the default" << endl;
+    container_types.push_back( new Container_Type() );
+    cout << "container_type:( " << *container_types.back() << endl;
+    cout << "container_type->class_name(): " << container_types.back()->class_name() << endl;
+  }
+
+  if( args.output_given ){
+    xmlDocPtr doc = xmlNewDoc( ( xmlChar* )( "1.0" ) );
+    xmlNodePtr root = xmlNewDocNode( doc, NULL, ( xmlChar* )( "root" ), NULL );
+    xmlDocSetRootElement( doc, root );
+    for( unsigned int i = 0; i < container_types.size(); i++ ){
+      container_types[ i ]->to_xml( doc, root );
+    }
+    xmlSaveFormatFileEnc( args.output_arg, doc, "UTF-8", 1 );
+    xmlFreeDoc( doc );
+  }
+
+  //memory management
+  for( unsigned int i = 0; i < container_types.size(); i++ ){
+    if( container_types[ i ] != NULL ){
+      delete container_types[ i ];
+      container_types[ i ] = NULL;
+    }
+  }
+
+  cout << "end of Container_Type class demo program" << endl;
+  return 0;
+}
