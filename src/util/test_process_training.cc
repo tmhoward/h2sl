20f9c3469da195543a667d8b8937bebaa53c2e6a
Jake Arkin <jarkin@ur.rochester.edu>
2017-03-28 14:18:26 -0400

    test_process_training first draft

--- src/util/test_process_training.cc
+++ src/util/test_process_training.cc
@@ -3,19 +3,16 @@
  *
  * @brief
  *
- * Test process. Uses a split training and test data set.
- * Runs the DCG and ADCG (ADCG) models and evaluates
- * Comparative accuracies. 
+ * Test Process Training. Uses provided training data partitions.
+ * Generates trained log-linear models.
  * 
- * The cumulated results and 
- * individual grounding results are written out.
- * 
- * Done for both root and complete accuracy metrics. 
+ * The produced log-linear models are
+ * stored as XML and appended to the end 
+ * of the corresponding input file
  *
- * ToDo: also add the H-DCG and H-ADCG models for evaluation.
  */
 
-#include "test_process_cmdline.h"
+#include "test_process_training_cmdline.h"
 #include <boost/filesystem/path.hpp>
 
 #include <assert.h>
@@ -53,10 +50,10 @@
 #include "h2sl/constraint.h"
 #include "h2sl/spatial_relation.h"
 
-#include "h2sl/dcg.h"
-#include "h2sl/hdcg.h"
-#include "h2sl/adcg.h"
-#include "h2sl/hadcg.h"
+//#include "h2sl/dcg.h"
+//#include "h2sl/hdcg.h"
+//#include "h2sl/adcg.h"
+//#include "h2sl/hadcg.h"
 
 using namespace std;
 using namespace h2sl; 
@@ -75,9 +72,7 @@ evaluate_model( LLM* llm,
   for( unsigned int i = 0; i < examples.size(); i++ ){
     vector< pair< vector< Feature* >, unsigned int > > features;
     double pygx = llm->pygx( examples[ i ].first, examples[ i ].second, cvs, features );
-    //if( pygx < 0.75 ){
-    if( false ){
-//    if( examples[ i ].first == "true" ){
+    if( pygx < 0.75 ){
       cout << "example " << i << " had pygx " << pygx << endl;
       cout << "   filename:\"" << examples[ i ].second.filename() << "\"" << endl;
       cout << "         cv:" << examples[ i ].first << endl;
@@ -132,8 +127,7 @@ main( int argc,
   /********** Train a llm for each provided training_set partition && 
               save it to a new xml file that also contains the data partitions *****************/
   for( unsigned int i = 0; i < args.inputs_num; i++ ){
-
-    // Vectors store list of training set filenames
+    // Vector to store list of training set filenames
     vector< string > training_set;
 
     // XML pointers
@@ -165,10 +159,13 @@ main( int argc,
         }
       }
       
-     /******************** Report ******************************************/
-     // Test number (partition of the corpus), training set size.
-      cout << "test " << i << endl << endl;
-      cout << "training_set[" << training_set.size() << "]:{";
+      /******************** Report ******************************************/
+      // Test number (partition of the corpus), training set size.
+      // get the test number from the filename
+      string filename = args.inputs[ i ];
+      string test_number_string = filename.substr( ( filename.size() - 8 ), 4 );
+      cout << "test " << test_number_string << endl;
+      cout << "  training_set[" << training_set.size() << "]:{";
       for( unsigned int j = 0; j < training_set.size(); j++ ){
         cout << training_set[ j ];
         if( j != ( training_set.size() - 1 ) ){
@@ -178,53 +175,8 @@ main( int argc,
       cout << "}" << endl;
    
       /********************** Train the Log-linear model *****************/ 
-      vector< Search_Space* > training_search_spaces_groundings( training_set.size(), NULL );
-      vector< Search_Space* > training_search_spaces_rules( training_set.size(), NULL );
-
-      vector< Phrase* > truth_training_phrases( training_set.size(), NULL );
-      vector< World* > training_worlds( training_set.size(), NULL );
-      vector< string > training_filenames( training_set.size() );
-
-      Symbol_Dictionary * symbol_dictionary_groundings = new Symbol_Dictionary( args.symbol_dictionary_groundings_arg );
-      Symbol_Dictionary * symbol_dictionary_rules = new Symbol_Dictionary( args.symbol_dictionary_rules_arg );
-     
       // Load examples from the training set for training the log-linear model.  
       vector< pair< string, LLM_X > > examples;
-      for( unsigned int j = 0; j < training_set.size(); j++ ){
-        // Get the filename.
-        training_filenames[ j ] = training_set[ j ];
-        cout << "reading file " << training_set[ j ] << endl;
-
-        // Load world
-        training_worlds[ j ] = new World();
-        training_worlds[ j ]->from_xml( training_set[ j ] );
-
-        // Load phrase
-        truth_training_phrases[ j ] = new Phrase();
-        truth_training_phrases[ j ]->from_xml( training_set[ j ], training_worlds[ j ] );
-
-        /************ Search Space fill rules and scrape examples ************************/
-        if( truth_training_phrases[ j ]->contains_symbol_in_symbol_dictionary( *symbol_dictionary_rules ) ){
-          cout << "contains symbols in rules symbol dictionary" << endl;
-          training_search_spaces_rules[ j ] = new Search_Space();
-          training_search_spaces_rules[ j ]->fill_rules( *symbol_dictionary_rules, training_worlds[ j ] );
-          training_search_spaces_rules[ j ]->scrape_examples( training_filenames[ j ], static_cast< Phrase* >( truth_training_phrases[ j ] ), training_worlds[ j ], examples );
-        } else {
-          cout << "does not contains any rules symbols in symbol dictionary" << endl;
-        }
-
-        /************ Search Space fill groundings and scrape examples. Note: examples is common. ***/
-        // Check if the phrase has symbols from the symbol dictionary groundings.
-        if( truth_training_phrases[ j ]->contains_symbol_in_symbol_dictionary( *symbol_dictionary_groundings ) ){
-          cout << "contains symbols in symbol dictionary" << endl;
-          training_search_spaces_groundings[ j ] = new Search_Space();
-          training_search_spaces_groundings[ j ]->fill_groundings( *symbol_dictionary_groundings, training_worlds[ j ] );
-          training_search_spaces_groundings[ j ]->scrape_examples( training_filenames[ j ], static_cast< Phrase* >( truth_training_phrases[ j ] ), training_worlds[ j ], examples );
-        } else {
-          cout << "does not contains any symbols in symbol dictionary" << endl;
-        }
-
-      }
       cout << "training with " << examples.size() << " examples" << endl;
 
       // Feature sets in multiple threads.
@@ -236,9 +188,6 @@ main( int argc,
           cout << "num features:" << feature_sets.back()->size() << endl;
         }
       }
-      if( !feature_sets.empty() ){
-       cout << "num features:" << feature_sets.front()->size() << endl;
-      }
 
       // Add feature sets to LLMs.
       vector< LLM* > llms;
@@ -249,86 +198,34 @@ main( int argc,
 
       LLM_Train* llm_train = new LLM_Train( llms );
 
-      // Main LLM Training call.
+      // Train the model && record the timing information.
       uint64_t train_start_time = current_time();
       llm_train->train( examples, args.max_iterations_arg, args.lambda_arg, args.epsilon_arg );
       int64_t train_end_time = current_time();
       double train_time = microseconds_to_seconds( train_end_time - train_start_time );
 
-      /********* Write out the LLM   **************/
-      stringstream llm_filename_string;
-      if( !args.solution_directory_given ){
-        llm_filename_string << "/tmp/llm_" << setw( 4 ) << setfill( '0' ) << i << ".xml";
-      } else{
-        llm_filename_string << solution_directory.str() << "/llm_" << setw( 4 ) << setfill( '0' ) << i << ".xml";
-      }
+      /********* Append the LLM to the input file and save as new file**************/
+      // append the llm to the xml document object
+      llms.front()->to_xml( input_doc, input_root );
+
+      // create the name for the new file
+      // test_number_string comes from the "report" code above
+      stringstream input_file_with_llm;
+      input_file_with_llm << args.output_arg << "/test_with_llm" << test_number_string << ".xml";
       
-      // write the llm to xml
-      llms.front()->to_xml( llm_filename_string.str() );
-
-      stringstream training_set_size_string;
-      training_set_size_string << training_set.size();
-      xmlNewProp( test_node, ( const xmlChar* )( "training_set_size" ), ( const xmlChar* )( training_set_size_string.str().c_str() ) );
-
-      stringstream test_set_size_string;
-      test_set_size_string << test_set.size();
-      xmlNewProp( test_node, ( const xmlChar* )( "test_set_size" ), ( const xmlChar* )( test_set_size_string.str().c_str() ) );
-
-      stringstream training_ratio_string;
-      training_ratio_string << ( double )( training_set.size() ) / ( double )( training_set.size() + test_set.size() );
-      xmlNewProp( test_node, ( const xmlChar* )( "training_ratio" ), ( const xmlChar* )( training_ratio_string.str().c_str() ) );
-
-      stringstream train_time_string;
-      train_time_string << train_time;
-      xmlNewProp( test_node, ( const xmlChar* )( "train_time" ), ( const xmlChar* )( train_time_string.str().c_str() ) );
+      //save the file
+      xmlSaveFormatFileEnc( input_file_with_llm.str().c_str(), input_doc, "UTF-8", 1 );
+      
 
       /********************* Evaluate the log-linear model ******************/
       cout << "evaluating llm model" << endl;
       evaluate_model( llms.front(), examples );
  
-      /********************* Run Inference Tests******************/
-      // Evaluate the DCG and ADCG models on the training set (if option).
-      if (args.test_training_set_arg) {
-        run_tests( training_set, llms.front(), tests_doc, test_node, "training_set", i, solution_directory.str(),
-                   args.symbol_dictionary_groundings_arg, args.beam_width_arg, args.debug_arg );
-        cout << "training_ratio:" << training_ratio_string.str() << endl << endl;
-      }
-
-      // Evaluate the DCG and ADCG models on the test set (if option).
-      if (args.test_test_set_arg) {
-        run_tests( training_set, llms.front(), tests_doc, test_node, "test_set", i, solution_directory.str(),
-                   args.symbol_dictionary_groundings_arg, args.beam_width_arg, args.debug_arg );
-        cout << "training_ratio:" << training_ratio_string.str() << endl << endl;
-      }
-     
-      xmlAddChild( tests_node, test_node );
-
       /**** Clean up  *****/
       if( llm_train != NULL ){
         delete llm_train;
         llm_train = NULL;
       }
-
-      if( symbol_dictionary_groundings != NULL ){
-        delete symbol_dictionary_groundings;
-        symbol_dictionary_groundings = NULL;
-      }
-
-      for( unsigned int i = 0; i < training_search_spaces_groundings.size(); i++ ){
-        if( training_search_spaces_groundings[ i ] != NULL ){
-          delete training_search_spaces_groundings[ i ];
-          training_search_spaces_groundings[ i ] = NULL;
-        }
-      }
-      training_search_spaces_groundings.clear();
-
-      for( unsigned int i = 0; i < training_search_spaces_rules.size(); i++ ){
-        if( training_search_spaces_rules[ i ] != NULL ){
-          delete training_search_spaces_rules[ i ];
-          training_search_spaces_rules[ i ] = NULL;
-        }
-      }
-      training_search_spaces_rules.clear();
 
       for( unsigned int i = 0; i < llms.size(); i++ ){
         if( llms[ i ] != NULL ){
@@ -346,40 +243,14 @@ main( int argc,
       }
       feature_sets.clear();
 
-      for( unsigned int i = 0; i < truth_training_phrases.size(); i++ ){
-        if( truth_training_phrases[ i ] != NULL ){
-          delete truth_training_phrases[ i ];
-          truth_training_phrases[ i ] = NULL;
-        }
-      }
-      truth_training_phrases.clear();
-
-      for( unsigned int i = 0; i < training_worlds.size(); i++ ){
-        if( training_worlds[ i ] != NULL ){
-          delete training_worlds[ i ];
-          training_worlds[ i ] = NULL;
-        }
-      }
-      training_worlds.clear();
-
-      // Write output file.
-      // get the test number from the filename
-      string filename = args.inputs[ i ];
-      string test_number_string = filename.substr( ( filename.size() - 8 ), 4 );
-      // write out the output
-      stringstream results_filename;
-      results_filename << args.output_arg << "/result_" << test_number_string << ".xml";
-      xmlSaveFormatFileEnc( results_filename.str().c_str(), tests_doc, "UTF-8", 1 );
-    } else {
-      cout << "could not read filename \"" << args.inputs[ i ] << "\"" << endl;
-      assert( false );
+    }
+    //free the xml pointers
+    if( input_doc != NULL ){
+      xmlFreeDoc( input_doc );
     }
   }
 
-  // Free the XML test_doc
-  xmlFreeDoc( tests_doc );
-
-  cout << "end of test_process program" << endl;
+  cout << "end of test_process_training program" << endl;
   return 0;
 }
 
