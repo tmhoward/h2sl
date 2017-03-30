fcd3a2789837677d32a2a130265b00e7717fbe45
rohanpaul <rohanp@csail.mit.edu>
2017-03-30 19:18:55 -0400

    test process inference script

---
+++ scripts/run_test_process_inference.sh
@@ -0,0 +1,12 @@
+#!/bin/bash          
+
+mkdir ../data/results
+
+mkdir ../data/results/results_0000
+
+mkdir ../data/results/results_0000/solutions
+
+mkdir -p ../data/cross_validation_partitions/combined_nav_corpora/collated_0000
+
+
+#../build/bin/h2sl-test-process-inference --feature_set ../data/training/navigation_corpus/combined/feature_set_groundings_combined.xml --output ../data/results/results_0000/ --solution_directory ../data/results/results_0000/solutions/ ../data/cross_validation_partitions/combined_nav_corpora/collated_0000/test_collated_0000.xml
