a70d7de1ad3c9388249a73fbb96ad47353c9beed
rohanpaul <rohanp@csail.mit.edu>
2017-03-30 22:20:02 -0400

    added model selection

--- scripts/run_test_process_inference.sh
+++ scripts/run_test_process_inference.sh
@@ -8,5 +8,4 @@ mkdir ../data/results/results_0000/solutions
 
 mkdir -p ../data/cross_validation_partitions/combined_nav_corpora/collated_0000
 
-
-#../build/bin/h2sl-test-process-inference --feature_set ../data/training/navigation_corpus/combined/feature_set_groundings_combined.xml --output ../data/results/results_0000/ --solution_directory ../data/results/results_0000/solutions/ ../data/cross_validation_partitions/combined_nav_corpora/collated_0000/test_collated_0000.xml
+../build/bin/h2sl-test-process-inference --model dcg --feature_set ../data/training/navigation_corpus/combined/feature_set_groundings_combined.xml --output ../data/results/results_0000/ --solution_directory ../data/results/results_0000/solutions/ ../data/cross_validation_partitions/combined_nav_corpora/collated_0000/test_collated_0000.xml
