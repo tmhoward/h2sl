ee442c90e9a8978a7021f3b5bf7f63051782ddc5
rohanpaul <rohanp@csail.mit.edu>
2017-04-02 21:15:37 -0400

    search space fix

--- scripts/exp_search_space_tests.sh
+++ scripts/exp_search_space_tests.sh
@@ -11,7 +11,4 @@ echo Search Space Test
 #../build/bin/h2sl-adcg-test ../data/training/navigation_corpus/objects_in_groups/example_0086.xml --symbol_dictionary_groundings ../data/training/navigation_corpus/combined/symbol_dictionary_groundings_combined.xml --llm ../data/training/navigation_corpus/combined/llm_combined.xml --debug 1 --beam_width 1
 
 # Run on the full data set.
-../build/bin/h2sl-test-process-inference --model adcg --feature_set ../data/training/navigation_corpus/combined/feature_set_groundings_combined.xml --output ../data/results/results_0000/ --solution_directory ../data/results/results_0000/solutions/ ../data/cross_validation_partitions/combined_nav_corpora/collated_0000/test_collated_0130.xml
-
-
-
+../build/bin/h2sl-test-process-inference --model adcg --feature_set ../data/training/navigation_corpus/combined/feature_set_groundings_combined.xml --output ../data/results/results_0001/ --solution_directory ../data/results/results_0001/solutions/ ../data/cross_validation_partitions/combined_nav_corpora/collated_0000/test_collated_0130.xml
