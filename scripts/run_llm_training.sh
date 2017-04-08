15aa49347a6274ea1b5c5c7f3c6b455b86ffcf55
rohanpaul <rohanp@csail.mit.edu>
2017-04-08 18:06:27 -0400

    script changes

--- scripts/run_llm_training.sh
+++ scripts/run_llm_training.sh
@@ -1,5 +1,5 @@
 #!/bin/bash          
 
 echo LLM Training 
-../build/bin/h2sl-llm-train-groundings ../data/training/navigation_corpus/groups/example_00*.xml ../data/training/navigation_corpus/objects_in_groups/example_00*.xml --feature_set ../data/training/navigation_corpus/combined/feature_set_groundings_combined.xml --symbol_dictionary_groundings ../data/training/navigation_corpus/combined/symbol_dictionary_groundings_combined.xml --output ../data/training/navigation_corpus/combined/llm_combined.xml
+../build/bin/h2sl-llm-train-groundings ../data/training/navigation_corpus/groups/example_00*.xml ../data/training/navigation_corpus/objects_in_groups/example_00*.xml --feature_set ../data/training/navigation_corpus/combined/feature_set_groundings_combined.xml --sd ../data/training/navigation_corpus/combined/symbol_dictionary_groundings_combined.xml --output ../data/training/navigation_corpus/combined/llm_combined.xml
 
