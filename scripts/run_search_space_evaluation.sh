7a26282e439f0df26cf3f1dbf8f6a150338f4b81
rohanpaul <rohanp@csail.mit.edu>
2017-04-10 11:19:51 -0400

    search space eval script

---
+++ scripts/run_search_space_evaluation.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+../build/bin/h2sl-search-space-corpus-analysis ../data/results/results-dcg-single/results/result_full.xml ../data/results/results-adcg-single/result_full.xml --output_script evaluation_search_space.m --output_text evaluation_search_space.txt
+
