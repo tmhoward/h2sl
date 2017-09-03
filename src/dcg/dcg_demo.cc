562bb780a5192ed11fc24e4a802f79f68e531145
rohanpaul <rohanp@csail.mit.edu>
2017-09-02 21:58:20 -0400

    updated readme to use sd abbreviation and added search space construction step in the dcg demo program

--- src/dcg/dcg_demo.cc
+++ src/dcg/dcg_demo.cc
@@ -86,8 +86,10 @@ main( int argc,
 
     phrase->to_xml( "/tmp/phrase.xml" ); 
 
+    // By default option is "all"
     search_space->fill_groundings( *symbol_dictionary, world );
-
+    cout << "search_space:" << *search_space << endl;
+  
     gettimeofday( &start_time, NULL );
 
     dcg->leaf_search( phrase, *symbol_dictionary, search_space, world, context, llm, args.beam_width_arg, ( bool )( args.debug_arg ) );
@@ -138,6 +140,10 @@ main( int argc,
           cout << "phrases[" << i << "]:" << *phrases[ i ] << endl;
     
           gettimeofday( &start_time, NULL );
+          
+          // By default "all"
+          search_space->fill_groundings( *symbol_dictionary, world );
+          cout << "search_space:" << *search_space << endl;
 
           dcg->leaf_search( phrases[ i ], *symbol_dictionary, search_space, world, context, llm, args.beam_width_arg, ( bool )( args.debug_arg ) );
 
