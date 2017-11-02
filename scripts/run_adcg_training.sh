71f52f8b4874d4d9c0f378059f36ebd129f1b95e
rohanpaul <rohanp@csail.mit.edu>
2017-11-02 14:14:18 -0400

    cmd line parameter change

--- scripts/run_adcg_training.sh
+++ scripts/run_adcg_training.sh
@@ -12,13 +12,9 @@ echo ADCG Training
 
 #============================ Generate feature sets. 
 # Groups data set alone.
-#../build/bin/h2sl-generate-feature-set-groundings ../data/training/navigation_corpus/groups/example_00*.xml --symbol_dictionary_groundings ../data/training/navigation_corpus/groups/symbol_dictionary_groundings.xml --output ../data/training/navigation_corpus/groups/feature_set_adcg.xml
 
 # Combined data set.
-../build/bin/h2sl-generate-feature-set-groundings ../data/training/navigation_corpus/groups/example_00*.xml ../data/training/navigation_corpus/objects_in_groups/example_00*.xml --symbol_dictionary_groundings symbol_dictionary.xml --output feature_set.xml
+../build/bin/h2sl-generate-feature-set-groundings ../data/training/navigation_corpus/groups/example_00*.xml ../data/training/navigation_corpus/objects_in_groups/example_00*.xml --sd symbol_dictionary.xml --output feature_set.xml
 
 # LLM Train 
-
-# ../build/bin/h2sl-llm-train-groundings ../data/training/navigation_corpus/groups/example_00*.xml --feature_set ../data/training/navigation_corpus/groups/feature_set_adcg.xml --symbol_dictionary_groundings ../data/training/navigation_corpus/groups/symbol_dictionary_groundings.xml --output ../data/training/navigation_corpus/groups/llm_adcg.xml
-
-../build/bin/h2sl-llm-train-groundings ../data/training/navigation_corpus/groups/example_00*.xml ../data/training/navigation_corpus/objects_in_groups/example_00*.xml --symbol_dictionary_groundings symbol_dictionary.xml --feature_set feature_set.xml --max_iterations 50 --output llm.xml
+../build/bin/h2sl-llm-train-groundings ../data/training/navigation_corpus/groups/example_00*.xml ../data/training/navigation_corpus/objects_in_groups/example_00*.xml --sd symbol_dictionary.xml --feature_set feature_set.xml --max_iterations 50 --output llm.xml
